d6f0adc8395e6bea1909d3a17660cac21cfc5fb7
Brian Birmingham <brian.birmingham@odysseysr.com>
2022-02-24 13:54:47 -0600

    message

--- MorleyPlotting/Circles_Vertices_ExteriorLines_InteriorAngles.py
+++ MorleyPlotting/Circles_Vertices_ExteriorLines_InteriorAngles.py
@@ -4,7 +4,8 @@
 #  1.  Draw circles
 #  2.  Draw vertex text
 #  3.  Draw exterior lines
-
+#  4.  Get interior angles
+#  5.  Get angle from horizontal
 
 #  Then, on_motion, re-draw everybody
 
@@ -14,6 +15,8 @@ import pdb
 import math
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+pi = math.acos(-1.0)
+radian_to_degrees = 360.0 / (2.0 * pi)
 
 class Point(object):
     def __init__(self, x, y):
@@ -23,12 +26,37 @@ class Point(object):
 def Vertex_text(x, y):
     return f'({x:.1f},{y:.1f})'
 
+def GetInteriorAngle(i,j):
+    # compute vectors from lines
+    a1_x = lines[i].get_xdata()[1] - lines[i].get_xdata()[0]
+    a1_y = lines[i].get_ydata()[1] - lines[i].get_ydata()[0]
+    # must change direction of second vector
+    a2_x = -lines[j].get_xdata()[1] + lines[j].get_xdata()[0]
+    a2_y = -lines[j].get_ydata()[1] + lines[j].get_ydata()[0]
+    dot_product = (a1_x * a2_x) + (a1_y * a2_y)
+    a1_mag = math.sqrt(a1_x*a1_x + a1_y*a1_y)
+    a2_mag = math.sqrt(a2_x*a2_x + a2_y*a2_y)
+    cosine = dot_product / (a1_mag * a2_mag)
+    theta = math.acos(cosine)
+    return theta * radian_to_degrees
+
+def GetAngleFromHorizontal(i):
+    a_x = lines[i].get_xdata()[1] - lines[i].get_xdata()[0]
+    a_y = lines[i].get_ydata()[1] - lines[i].get_ydata()[0]
+    dot_product = a_x
+    a_mag = math.sqrt(a_x*a_x + a_y*a_y)
+    cosine = dot_product / a_mag
+    theta = math.acos(cosine)
+    return theta * radian_to_degrees
+
 class MoveableCircle(object):
-    def __init__(self, circle, vertices, vertex_text, lines):
+    def __init__(self, circle, vertices, vertex_text, lines, interior_angles_text, angle_from_horizontal_text):
         self.circle = circle
         self.vertices = vertices
         self.vertex_text = vertex_text
         self.lines = lines
+        self.interior_angles_text = interior_angles_text
+        self.angle_from_horizontal_text = angle_from_horizontal_text
         self.press = None
 
     def redraw_line(self, i, j, x, y):
@@ -109,7 +137,27 @@ class MoveableCircle(object):
         elif index == 2:  # re-draw lines 2 and 3
             self.redraw_line(1, 2, x, y)
 
-        #  4.  Get interior angles
+        #  4.  Re-compute interior angles
+        angle = GetInteriorAngle(0,2)
+        text = f'Theta13 = {angle:.2f}'
+        self.interior_angles_text[0].set_text(text)
+        angle = GetInteriorAngle(1, 0)
+        text = f'Theta21 = {angle:.2f}'
+        self.interior_angles_text[1].set_text(text)
+        angle = GetInteriorAngle(2, 1)
+        text = f'Theta32 = {angle:.2f}'
+        self.interior_angles_text[2].set_text(text)
+
+        #  5.  Re-compute angle from horizontal
+        angle = GetAngleFromHorizontal(0)
+        text = f'Alpha1 = {angle:.2f}'
+        self.angle_from_horizontal_text[0].set_text(text)
+        angle = GetAngleFromHorizontal(1)
+        text = f'Alpha2 = {angle:.2f}'
+        self.angle_from_horizontal_text[1].set_text(text)
+        angle = GetAngleFromHorizontal(2)
+        text = f'Alpha3 = {angle:.2f}'
+        self.angle_from_horizontal_text[2].set_text(text)
 
 
         self.circle.figure.canvas.draw()
@@ -124,6 +172,7 @@ circles = []        # circles are plt.Circle()
 vertex_text = []    # vertex_text are plt.text
 lines = []          # lines are the exterior (1-3) and interior lines (4-9)
 interior_angles_text = []  # interior_angles_text are plt.text
+angle_from_horizontal_text = []  # the angle each exterior line makes with horizontal
 
 fig = plt.figure(figsize=(16,10))
 axes = plt.axes()
@@ -149,19 +198,19 @@ vertices.append(w)
 
 circle1 = plt.Circle((vertices[0].x,  vertices[0].y), radius, fc="blue", label="unus")
 plt.gca().add_patch(circle1)
-circle = MoveableCircle(circle1, vertices, vertex_text, lines)
+circle = MoveableCircle(circle1, vertices, vertex_text, lines, interior_angles_text, angle_from_horizontal_text)
 circle.connect()
 circles.append(circle)
 
 circle2 = plt.Circle((vertices[1].x,  vertices[1].y), radius, fc="blue", label="duo")
 plt.gca().add_patch(circle2)
-circle = MoveableCircle(circle2, vertices, vertex_text, lines)
+circle = MoveableCircle(circle2, vertices, vertex_text, lines, interior_angles_text, angle_from_horizontal_text)
 circle.connect()
 circles.append(circle)
 
 circle3 = plt.Circle((vertices[2].x,  vertices[2].y), radius, fc="blue", label="tres")
 plt.gca().add_patch(circle3)
-circle = MoveableCircle(circle3, vertices, vertex_text, lines)
+circle = MoveableCircle(circle3, vertices, vertex_text, lines, interior_angles_text, angle_from_horizontal_text)
 circle.connect()
 circles.append(circle)
 
@@ -174,11 +223,11 @@ vertex_text1 = plt.text(0.01, 0.98, text, fontsize=font_size, transform=axes.tra
 vertex_text.append(vertex_text1)
 
 text = Vertex_text(vertices[1].x, vertices[1].y)
-vertex_text2 = plt.text(0.93, 0.98, text, fontsize=font_size, transform=axes.transAxes)
+vertex_text2 = plt.text(0.88, 0.98, text, fontsize=font_size, transform=axes.transAxes)
 vertex_text.append(vertex_text2)
 
 text = Vertex_text(vertices[2].x, vertices[2].y)
-vertex_text3 = plt.text(0.46, 0.05, text, fontsize=font_size, transform=axes.transAxes)
+vertex_text3 = plt.text(0.46, 0.09, text, fontsize=font_size, transform=axes.transAxes)
 vertex_text.append(vertex_text3)
 
 
@@ -195,6 +244,43 @@ lines.append(line2)
 line3 = plt.Line2D([w.x, u.x],[w.y, u.y], color='black', linewidth = 4)
 plt.gca().add_line(line3)
 lines.append(line3)
+
+
+#########################  interior angles 1,2,3  ###################################
+#  4.  Get interior angles
+angle = GetInteriorAngle(0,2)
+text = f'Theta13 = {angle:.2f}'
+interior_angles_text1 = plt.text(0.01, 0.94, text, fontsize=font_size, transform=axes.transAxes)
+interior_angles_text.append(interior_angles_text1)
+
+angle = GetInteriorAngle(1, 0)
+text = f'Theta21 = {angle:.2f}'
+interior_angles_text1 = plt.text(0.88, 0.94, text, fontsize=font_size, transform=axes.transAxes)
+interior_angles_text.append(interior_angles_text1)
+
+angle = GetInteriorAngle(2, 1)
+text = f'Theta32 = {angle:.2f}'
+interior_angles_text1 = plt.text(0.46, 0.05, text, fontsize=font_size, transform=axes.transAxes)
+interior_angles_text.append(interior_angles_text1)
+
+
+#########################  angle from horizontal 1,2,3  ###################################
+#  5.  Get angle from horizontal
+angle = GetAngleFromHorizontal(0)
+text = f'Alpha1 = {angle:.2f}'
+angle_from_horizontal_text1 = plt.text(0.01, 0.90, text, fontsize=font_size, transform=axes.transAxes)
+angle_from_horizontal_text.append(angle_from_horizontal_text1)
+
+angle = GetAngleFromHorizontal(1)
+text = f'Alpha2 = {angle:.2f}'
+angle_from_horizontal_text1 = plt.text(0.88, 0.90, text, fontsize=font_size, transform=axes.transAxes)
+angle_from_horizontal_text.append(angle_from_horizontal_text1)
+
+angle = GetAngleFromHorizontal(2)
+text = f'Alpha3 = {angle:.2f}'
+angle_from_horizontal_text1 = plt.text(0.46, 0.01, text, fontsize=font_size, transform=axes.transAxes)
+angle_from_horizontal_text.append(angle_from_horizontal_text1)
+
 
 
 plt.show()
